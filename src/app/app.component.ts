34bb907deb8f753b6ffb35fa84da0f5a176d587d
Tin Trinh <trinh.tin@hamee.co.jp>
2020-06-21 00:43:45 +0900

    Add todo list item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngrx-todo-list';
+
+  todoList = [
+    {
+      id: 1,
+      title: 'Do home work',
+    },
+    {
+      id: 2,
+      title: 'Do home work 2',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputButtonComponent } from './component/input-button/input-button.component';
+import { TodoItemComponent } from './component/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputButtonComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
