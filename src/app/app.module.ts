8bdb2a22f8c69de59fbe097fd4f1dfef08cfa72b
Tin Trinh <trinh.tin@hamee.co.jp>
2020-06-21 00:44:41 +0900

    Remove deleted component from declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InputButtonComponent } from './component/input-button/input-button.component';
 import { TodoItemComponent } from './component/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonComponent,
     TodoItemComponent
   ],
   imports: [
